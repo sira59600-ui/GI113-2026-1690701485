c564cdbfa940a88e1585d91e9ce8863b228b1e32
Coffee_Time <sira59600@gmail.com>
2026-08-25 03:35:26 -0700

    Add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bossName = "Supakit";
+            char rank = 'F';
+            int level = 10;
+            int maxHp = 100;
+            int CurrentHp = 100;
+            float attackPower = 27.5f;
+            double critMultiplier = 1.25;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine();
+
+            int hpPercentage = (CurrentHp * 100) / maxHp;
+            Console.WriteLine($"HP Percent : {hpPercentage}%");
+            Console.WriteLine();
+            Console.WriteLine("Supakit take 20 damage!");
+            CurrentHp = CurrentHp - 20;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP : {CurrentHp}/{maxHp}");
+            int newhpPercentage = (CurrentHp * 100) / maxHp;
+            Console.WriteLine($"HP Percent : {newhpPercentage}%");
+
+        }
+    }
+}
