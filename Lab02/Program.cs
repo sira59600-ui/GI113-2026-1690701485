ff4a2da76f1929804a54b4b4ce1aebdf4bea8a09
Coffee_Time <sira59600@gmail.com>
2026-08-25 22:29:22 +0700

    Add Lab02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -4,24 +4,34 @@
     {
         static void Main(string[] args)
         {
-            string bossName = "Supakit";
-            char rank = 'F';
-            int level = 10;
-            int maxHp = 100;
-            int CurrentHp = 100;
-            float attackPower = 27.5f;
-            double critMultiplier = 1.25;
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int CurrentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
             bool isBoss = true;
 
             Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name : {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {CurrentHp} / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Critical Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+
             Console.WriteLine();
 
             int hpPercentage = (CurrentHp * 100) / maxHp;
             Console.WriteLine($"HP Percent : {hpPercentage}%");
             Console.WriteLine();
-            Console.WriteLine("Supakit take 20 damage!");
-            CurrentHp = CurrentHp - 20;
+            Console.WriteLine("Kirin take 60 damage!");
+            CurrentHp = CurrentHp - 60;
+
             Console.WriteLine();
+
             Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
             Console.WriteLine($"HP : {CurrentHp}/{maxHp}");
             int newhpPercentage = (CurrentHp * 100) / maxHp;
