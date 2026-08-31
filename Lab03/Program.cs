905c299cac544f8beb631ea4e729ead313557bb7
sira59600-ui <sira59600@gmail.com>
2026-08-31 16:24:28 +0700

    Add Lab03

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";
+            var rank = "S";
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"NAME: {bossName}" +
+            $"\nRANK: {rank}" +
+            $"\nLEVEL: {level} / {MaxLevel}" +
+            $"\nHP: {currentHp} / {maxHp}" +
+            $"\nATTACK POWER: {attackPower}" +
+            $"\nCRIT MULTIPLIER: {critMultiplier}" +
+            $"\nIS BOSS: {isBoss}");
+
+            // Implicit Conversion HP (int) -->> double
+            Console.WriteLine("\n----- Implicit Conversion: HP as double -----");
+            double currentHpDouble = currentHp;
+            Console.WriteLine($"HP (double): {currentHpDouble}");
+
+            //Calculate Percentage as double
+            Console.WriteLine("\n----- Exact HP Percent (no integer truncation) -----");
+            double hpPercentageExact = currentHpDouble * 100 / maxHp;
+            Console.WriteLine($"HP Percent (exact): {hpPercentageExact}%");
+
+            // Explicit casting attack power (float) -->> int
+
+            // Cast vs. Convert: Crit Multiplier
         }
     }
 }
