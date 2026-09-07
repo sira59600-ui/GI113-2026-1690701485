0363746f710c6b3b8d0bfbfd18ac8ef298e9e51a
sira59600-ui <sira59600@gmail.com>
2026-09-07 09:30:39 +0700

    Add Lab04

---
+++ Lab04/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("|      NEW ADVENTURE|      ");
+            Console.WriteLine("+------------------------+");
+            Console.Write("Name your hero: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"\n\"Welcome, {playerName}. Your journey begins...\"");
+
+            Console.Write("Choose difficulty (1-3): ");
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Difficulty set to {difficulty}");
+        }
+    }
+}
